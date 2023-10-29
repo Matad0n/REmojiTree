b1069227bb11c6db2b21b5e903b823656c35d4aa
Matadon <149233480+Matad0n@users.noreply.github.com>
2023-10-29 09:51:18 +0100

    Changed trees depending on emojitype
    🪴🥬 depict different taxa depending on the emoji type so different trees depending on the type were included.

--- REmojiTree.R
+++ REmojiTree.R
@@ -2,9 +2,9 @@ library(ggplot2)
 library(ggtree)
 
 #Define the directory you want your images in
-imgdir <- c("C:/")
-#Select type of emoji you want ("Apple", "Twitter", "Facebook", "Google", "WhatsApp", "Microsoft", "Samsung")
-emoji_type = "Twitter"
+imgdir <- c("C:/Users/gf004/Desktop/emoji")
+#Select type of emoji you want ("Apple", "Twitter", "Facebook", "Google", "WhatsApp", "Samsung")
+emoji_type = "Samsung"
 
 
 #Define names
@@ -19,7 +19,8 @@ names <- c(
   "carrot_1f955", "sunflower_1f33b", "blossom_1f33c", "green-apple_1f34f", "christmas-tree_1f384",
   "coconut_1f965", "garlic_1f9c4", "ginger_1fada", "hyacinth_1fabb", "lotus_1fab7", "onion_1f9c5",
   "pea-pod_1fadb", "beans_1fad8", "bell-pepper_1fad1", "blueberries_1fad0", "broccoli_1f966", "chocolate-bar_1f36b",
-  "four-leaf-clover_1f340", "leafy-green_1f96c", "mango_1f96d", "olive_1fad2", "cigarette_1f6ac"
+  "four-leaf-clover_1f340", "leafy-green_1f96c", "mango_1f96d", "olive_1fad2", "cigarette_1f6ac",
+  "potted-plant_1fab4", "hot-beverage_2615", "teacup-without-handle_1f375"
 )
 
 
@@ -29,7 +30,6 @@ url_patterns <- list(
   Twitter = "https://em-content.zobj.net/source/twitter/376/",
   Google = "https://em-content.zobj.net/source/google/350/",
   WhatsApp = "https://em-content.zobj.net/source/whatsapp/352/",
-  Microsoft = "https://em-content.zobj.net/source/microsoft/319/",
   Facebook = "https://em-content.zobj.net/source/facebook/355/",
   Samsung = "https://em-content.zobj.net/source/samsung/349/"
 )
@@ -46,15 +46,30 @@ for (name in names) {
   cat("Downloaded", simplified_filename, "emoji.\n")
 }
 
-nwk <- paste0("((evergreen-tree, christmas-tree),((avocado, lotus),((tulip, ((hyacinth, (garlic, onion)), ((palm-tree, coconut), ((banana, ginger), (pineapple, (ear-of-corn, (tanabata-tree, sheaf-of-rice))))))),((grapes, ((((peanuts, (beans, pea-pod)), (shamrock, four-leaf-clover)), (((rose, strawberry), (((red-apple, green-apple), pear), (peach, (cherry-blossom, cherries)))), (chestnut, (jack-o-lantern, (watermelon, (cucumber, melon)))))), (((hibiscus, chocolate-bar), broccoli), ((mango, ((tangerine, lemon), (maple-leaf))))))),(cactus, ((kiwi-fruit, blueberries), ((olive,(roasted-sweet-potato,(cigarette, ((hot-pepper, bell-pepper), (eggplant, (potato, tomato)))))), (carrot,(leafy-green, (sunflower, blossom))))))))));")
+# Generate Newick tree string based on emoji type
+generate_tree <- function(emoji_type) {
+  if (emoji_type == "Twitter") {
+    nwk <- paste0("(potted-plant, ((evergreen-tree, christmas-tree),((avocado, lotus),((tulip, ((hyacinth, (garlic, onion)), ((palm-tree, coconut), ((banana, ginger), (pineapple, (ear-of-corn, (tanabata-tree, sheaf-of-rice))))))),((grapes, ((((peanuts, (beans, pea-pod)), (shamrock, four-leaf-clover)), (((rose, strawberry), (((red-apple, green-apple), pear), (peach, (cherry-blossom, cherries)))), (chestnut, (jack-o-lantern, (watermelon, (cucumber, melon)))))), (((hibiscus, chocolate-bar), broccoli), ((mango, ((tangerine, lemon), (maple-leaf))))))),(cactus, ((teacup-without-handle, (kiwi-fruit, blueberries)), ((olive, hot-beverage,(roasted-sweet-potato,(cigarette, ((hot-pepper, bell-pepper), (eggplant, (potato, tomato)))))), (carrot, (leafy-green, (sunflower, blossom)))))))))));")
+  } else if (emoji_type == "WhatsApp") {
+    nwk <- paste0("((evergreen-tree, christmas-tree),((avocado, lotus),(((potted-plant, (tulip, ((hyacinth, (garlic, onion)), ((palm-tree, coconut), ((banana, ginger), (pineapple, (ear-of-corn, (tanabata-tree, sheaf-of-rice))))))))),((grapes, ((((peanuts, (beans, pea-pod)), (shamrock, four-leaf-clover)), (((rose, strawberry), (((red-apple, green-apple), pear), (peach, (cherry-blossom, cherries)))), (chestnut, (jack-o-lantern, (watermelon, (cucumber, melon)))))), (((hibiscus, chocolate-bar), broccoli), ((mango, ((tangerine, lemon), (maple-leaf))))))),(cactus, ((teacup-without-handle, (kiwi-fruit, blueberries)), ((olive, hot-beverage,(roasted-sweet-potato,(cigarette, ((hot-pepper, bell-pepper), (eggplant, (potato, tomato)))))), (carrot, (leafy-green, (sunflower, blossom))))))))));")
+  } else if (emoji_type == "Apple") {
+    nwk <- paste0("((evergreen-tree, christmas-tree),((avocado, lotus),(((tulip, ((hyacinth, (garlic, onion)), ((palm-tree, coconut), ((banana, ginger), (pineapple, (ear-of-corn, (tanabata-tree, sheaf-of-rice)))))))),((grapes, ((((peanuts, (beans, pea-pod)), (shamrock, four-leaf-clover)), (((rose, strawberry), (((red-apple, green-apple), pear), (peach, (cherry-blossom, cherries)))), (chestnut, (jack-o-lantern, (watermelon, (cucumber, melon)))))), (((hibiscus, chocolate-bar), (broccoli, leafy-green)), ((mango, ((tangerine, lemon), (maple-leaf))))))),(cactus, ((teacup-without-handle, (kiwi-fruit, blueberries)), ((olive, hot-beverage,(roasted-sweet-potato,(cigarette, ((hot-pepper, bell-pepper), (eggplant, (potato, tomato)))))), (carrot, (sunflower, blossom)))))))));")
+  } else if (emoji_type == "Google") {
+    nwk <- paste0("(potted-plant, ((evergreen-tree, christmas-tree),((avocado, lotus),(((tulip, ((hyacinth, (garlic, onion)), ((palm-tree, coconut), ((banana, ginger), (pineapple, (ear-of-corn, (tanabata-tree, sheaf-of-rice)))))))),((grapes, ((((peanuts, (beans, pea-pod)), (shamrock, four-leaf-clover)), (((rose, strawberry), (((red-apple, green-apple), pear), (peach, (cherry-blossom, cherries)))), (chestnut, (jack-o-lantern, (watermelon, (cucumber, melon)))))), (((hibiscus, chocolate-bar), (broccoli, leafy-green)), ((mango, ((tangerine, lemon), (maple-leaf))))))),(cactus, ((teacup-without-handle, (kiwi-fruit, blueberries)), ((olive, hot-beverage,(roasted-sweet-potato,(cigarette, ((hot-pepper, bell-pepper), (eggplant, (potato, tomato)))))), (carrot, (sunflower, blossom))))))))));")
+  } else if (emoji_type == "Facebook") {
+    nwk <- paste0("((evergreen-tree, christmas-tree),((avocado, lotus),(((potted-plant, (tulip, ((hyacinth, (garlic, onion)), ((palm-tree, coconut), ((banana, ginger), (pineapple, (ear-of-corn, (tanabata-tree, sheaf-of-rice))))))))),((grapes, ((((peanuts, (beans, pea-pod)), (shamrock, four-leaf-clover)), (((rose, strawberry), (((red-apple, green-apple), pear), (peach, (cherry-blossom, cherries)))), (chestnut, (jack-o-lantern, (watermelon, (cucumber, melon)))))), (((hibiscus, chocolate-bar), (broccoli,leafy-green)), ((mango, ((tangerine, lemon), (maple-leaf))))))),(cactus, ((teacup-without-handle, (kiwi-fruit, blueberries)), ((olive, hot-beverage,(roasted-sweet-potato,(cigarette, ((hot-pepper, bell-pepper), (eggplant, (potato, tomato)))))), (carrot, (sunflower, blossom)))))))));")
+  } else if (emoji_type == "Samsung") {
+    nwk <- paste0("((evergreen-tree, christmas-tree),((avocado, lotus),(((potted-plant, (tulip, ((hyacinth, (garlic, onion)), ((palm-tree, coconut), ((banana, ginger), (pineapple, (ear-of-corn, (tanabata-tree, sheaf-of-rice))))))))),((grapes, ((((peanuts, (beans, pea-pod)), (shamrock, four-leaf-clover)), (((rose, strawberry), (((red-apple, green-apple), pear), (peach, (cherry-blossom, cherries)))), (chestnut, (jack-o-lantern, (watermelon, (cucumber, melon)))))), (((hibiscus, chocolate-bar), (broccoli,leafy-green)), ((mango, ((tangerine, lemon), (maple-leaf))))))),(cactus, ((teacup-without-handle, (kiwi-fruit, blueberries)), ((olive, hot-beverage,(roasted-sweet-potato,(cigarette, ((hot-pepper, bell-pepper), (eggplant, (potato, tomato)))))), (carrot, (sunflower, blossom)))))))));")
+  }
+  return(nwk)
+}
 
+# Get Newick tree string based on emoji type
+nwk <- generate_tree(emoji_type)
 tree = read.tree(text = nwk)
 
 p <- ggtree(tree, layout = "circular") +
   geom_tiplab(aes(image=paste0(imgdir, '/', label, '.png')), geom="image", offset=3, align = T, size=.05)
-
-#print
-p
 
 #save as png
 png(filename = file.path(imgdir, "emojitree.png"), width = 10, height = 10, unit = "cm", res = 1000)
